473bc6be2cccafdeb4d80a37133e889d2f41410a
Paul Yoder <paulyoder@gmail.com>
2011-10-14 09:54:51 -0500

    initial commit

---
+++ WebResources/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+[assembly: WebResource("WebResources.Styles.ResourceStyle.css", "text/css")]
+namespace WebResources
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var styleUrl = Page.ClientScript.GetWebResourceUrl(this.GetType(), "WebResources.Styles.ResourceStyle.css");
+            //var styleUrl = "/Styles/ResourceStyle.css";
+            var attribute = string.Format("<link href='{0}' type='text/css' rel='stylesheet' />", styleUrl);
+            var include = new LiteralControl(attribute);
+            ((System.Web.UI.HtmlControls.HtmlHead)Page.Header).Controls.Add(include);
+        }
+    }
+}
